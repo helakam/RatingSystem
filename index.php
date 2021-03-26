5218e3cf1eb387610c5f262df7e1fc9d26b1f0b8
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-03-26 12:54:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,4 +6,9 @@
     $ratedIndex = $conn->real_escape_string($_POST['ratedIndex']);
     $ratedIndex++;
 
+  if (!$uID) {
+    $conn->query("INSERT INTO stars (rateIndex) VALUES ('$ratedIndex')");
+    $sql = $conn->query("SELECT id FROM stars ORDER BY id DESC LIMIT 1");
+    ratedIndex++; 
+  }
 >
