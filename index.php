7788326d798725964874e7bb3bee7ecfef482001
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-03-26 08:57:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
   $conn = new mysqli('localhost', 'root', '', 'ratingSystem');
+  
+  if (isset($_POST['save'])) {
+    $uID = $conn->real_escape_string($_POST['uID']);
+    $ratedIndex = $conn->real_escape_string($_POST['ratedIndex']);
+    $ratedIndex++;
+
 >
